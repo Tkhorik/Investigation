af592c5eba61d63af540ad6ee44e48180428c458
Code <javasetraining@gmail.com>
2016-12-15 00:31:38 +0200

    finished lesion with sorting finished for tests coverage

--- src/main/java/ru/maven/chapter_001/Max.java
+++ src/main/java/ru/maven/chapter_001/Max.java
@@ -1,6 +1,10 @@
 package ru.maven.chapter_001;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 public class Max {
+
     public static void main(String[] args) {
         Max max = new Max();
         System.out.println(max.max(3, 2, 1));
@@ -8,11 +12,27 @@ public class Max {
         System.out.println(max.max(1, 2, 3));
     }
 
-    public int max(int one, int two, int three) {
+    int max(int one, int two, int three) {
         return max(one, max(two, three));
     }
 
-    public int max(int one, int two) {
+    private int max(int one, int two) {
         return one > two ? one : two;
     }
+
+    /*
+    //Method for define max in any array
+
+    public Max(final int[] values){
+        this.values = Arrays.copyOf(values, values.length);
+    }
+
+    public int max(){
+        int max = this.values[0];
+        for (int value : this.values){
+            max = this.max(max, value);
+        }
+     return max;
+    }
+ */
 }
--- src/test/java/ru/maven/chapter_001/MaxTest.java
+++ src/test/java/ru/maven/chapter_001/MaxTest.java
@@ -12,5 +12,15 @@ public class MaxTest {
         Max max = new Max();
         int result = max.max(3, 2, 1);
         Assert.assertThat("in {3,2,1} max 3", result, is(3));
+    }    @Test
+    public void whenMaxSecondThenReturnSecond() throws Exception {
+        Max max = new Max();
+        int result = max.max(2, 3, 1);
+        Assert.assertThat("in {3,2,1} max 3", result, is(3));
+    }    @Test
+    public void whenMaxThirdThenReturnThird() throws Exception {
+        Max max = new Max();
+        int result = max.max(1, 2, 3);
+        Assert.assertThat("in {3,2,1} max 3", result, is(3));
     }
 }
